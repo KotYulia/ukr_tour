1d347cde35018b8d9a46be061c32c4dcdc3ba135
KotYulia <julkotenko@gmail.com>
2019-04-10 12:59:51 +0300

    Added change_column :tours, :price, :integer, using: 'price::integer'

--- db/migrate/20190410095815_change_price_to_be_integer_in_tours.rb
+++ db/migrate/20190410095815_change_price_to_be_integer_in_tours.rb
@@ -1,9 +1,5 @@
 class ChangePriceToBeIntegerInTours < ActiveRecord::Migration[5.2]
-  def up
-    change_column :tours, :price, :integer
-  end
-
-  def down
+  def change
     change_column :tours, :price, :integer, using: 'price::integer'
   end
 end
